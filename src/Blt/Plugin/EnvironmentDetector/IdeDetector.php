0d6ced905219ab22297d7de21bb91ec8fd91865b
Pavlos Daniel <pavlos.daniel@acquia.com>
2021-02-03 02:51:02 +0000

    Adjust local environment detection to treat the Acquia IDE as such.

--- src/Blt/Plugin/EnvironmentDetector/IdeDetector.php
+++ src/Blt/Plugin/EnvironmentDetector/IdeDetector.php
@@ -5,17 +5,16 @@ namespace Acquia\BltIdeLocal\Blt\Plugin\EnvironmentDetector;
 use Acquia\Blt\Robo\Common\EnvironmentDetector;
 
 class IdeDetector extends EnvironmentDetector {
+
   /**
    * Is AH IDE.
    */
-  public static function isAhProdEnv() {
-    $ah_env = self::getAhEnv();
+  public static function isLocalEnv() {
+    // Check if we are on an Acquia Environment that is an IDE as well.
+    $is_local = (self::isAhEnv() && self::getAhEnv() == 'ide') ||
+      (!self::isAhEnv() && !self::isPantheonEnv() && !self::isCiEnv());
 
-    var_dump($ah_env);
-    die();
-
-    // ACE prod is 'prod'; ACSF can be '01live', '02live', ...
-    return $ah_env == 'prod' || preg_match('/^\d*live$/', $ah_env);
+    return $is_local;
   }
 
 }
